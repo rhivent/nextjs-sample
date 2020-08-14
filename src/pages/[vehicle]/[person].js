87c74840c4df37d6afed8ef26fa54bbb0e1bdbe7
rhivent <zzzzjrjr@gmail.com>
2020-08-14 16:08:11 +0700

    nextjs tutorial : video 1 => minutes 7'

--- src/pages/[vehicle]/[person].js
+++ src/pages/[vehicle]/[person].js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 const Person = () => {
   const router = useRouter();
   console.log(router.query);
-  return <h3>{router.query.person}'s car</h3>;
+  return <h3>{router.query.person}'s {router.query.vehicle}</h3>;
 };
 
 export default Person;
